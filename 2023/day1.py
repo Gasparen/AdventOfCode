406dbe4eb611a006434309bf2cfbb79e8fc0ef72
Jesper Axelsson <jesper.axelsson@sogeti.se>
2023-12-01 13:00:50 +0100

    Day 1 2023 - part 2

--- 2023/day1.py
+++ 2023/day1.py
@@ -4,6 +4,16 @@ def read_file():
     df = pd.read_csv(filepath_or_buffer='day1_indata.csv',header=None)
     return df
 
+digitDictionary = {'one': 'o1e',
+                   'two': 't2o',
+                   'three': 't3e',
+                   'four': '4',
+                   'five': '5e',
+                   'six': '6',
+                   'seven': '7n',
+                   'eight': 'e8t',
+                   'nine': 'n9e'}
+
 def filter_numbers(s: str):
     temp = []
     for c in s:
@@ -11,6 +21,13 @@ def filter_numbers(s: str):
             temp.append(c)
 
     return temp
+
+def tokenize(s: str):
+    returnString = s
+    for key, value in digitDictionary.items():
+        returnString = returnString.replace(key, value)
+
+    return returnString
 
 def concat_first_last(numbers):
     return int("" + numbers[0] + numbers[len(numbers)-1])
@@ -21,8 +38,12 @@ def main():
     df['onlyNumbers'] = df.apply(lambda row: filter_numbers(row['data']), axis=1)
     df['calibrationValue'] = df['onlyNumbers'].map(concat_first_last)
 
+    df['numbersAndWords'] = df.apply(lambda row: filter_numbers(tokenize(row['data'])), axis=1)
+    df['calibrationValueExtended'] = df['numbersAndWords'].map(concat_first_last)
+
     print(df)
     print(df['calibrationValue'].sum())
+    print(df['calibrationValueExtended'].sum())
 
 if __name__ == '__main__':
     main()
