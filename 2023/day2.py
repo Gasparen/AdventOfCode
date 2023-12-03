aabe4148ffcfe234fbd111fed0c9fb1c7ecb11dc
Jesper Axelsson <gasparen@gmail.com>
2023-12-04 00:06:46 +0100

    Fix for day 2

--- 2023/day2.py
+++ 2023/day2.py
@@ -9,26 +9,26 @@ maxDictionary = {'red':12,'green':13,'blue':14}
 
 def anyAboveMax(red, green, blue):
     return (
-            (max(red) > maxDictionary['red']) or 
-            (max(green) > maxDictionary['green']) or 
-            (max(blue) > maxDictionary['blue'])
+            (red > maxDictionary['red']) or 
+            (green > maxDictionary['green']) or 
+            (blue > maxDictionary['blue'])
             )
 
 def powerOfMinimumAmount(red, green,blue):
-    return max(red)*max(green)*max(blue)
+    return red*green*blue
 
 def main():
     df = read_file()
     df.columns = ['ID','data']
     df['ID'] = df['ID'].map(lambda x: x[5:])
-    df['reds'] = df['data'].map(lambda s: map(int, re.findall(r'(\d+) red', s)))
-    df['greens'] = df['data'].map(lambda s: map(int, re.findall(r'(\d+) green', s)))
-    df['blues'] = df['data'].map(lambda s: map(int, re.findall(r'(\d+) blue', s)))
-    #df['exceedsMax'] = df.apply(lambda row: anyAboveMax(row['reds'],row['greens'],row['blues']), axis=1)
-    df['powerOfMinimumAmount'] = df.apply(lambda row: powerOfMinimumAmount(row['reds'],row['greens'],row['blues']), axis=1)
+    df['maxRed'] = df['data'].map(lambda s: max(map(int, re.findall(r'(\d+) red', s))))
+    df['maxGreen'] = df['data'].map(lambda s: max(map(int, re.findall(r'(\d+) green', s))))
+    df['maxBlue'] = df['data'].map(lambda s: max(map(int, re.findall(r'(\d+) blue', s))))
+    df['exceedsMax'] = df.apply(lambda row: anyAboveMax(row['maxRed'],row['maxGreen'],row['maxBlue']), axis=1)
+    df['powerOfMinimumAmount'] = df.apply(lambda row: powerOfMinimumAmount(row['maxRed'],row['maxGreen'],row['maxBlue']), axis=1)
 
     print(df)
-    #print(df[df['exceedsMax'] == False]['ID'].map(int).sum())
+    print(df[df['exceedsMax'] == False]['ID'].map(int).sum())
     print(df['powerOfMinimumAmount'].sum())
 
 if __name__ == '__main__':
