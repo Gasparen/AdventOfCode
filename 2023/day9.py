f5abef71349d9408b20a57b84a7cb848fe243852
Jesper Axelsson <gasparen@gmail.com>
2023-12-20 23:43:20 +0100

    Advent of Code day 9 - Part 2

--- 2023/day9.py
+++ 2023/day9.py
@@ -16,6 +16,15 @@ def diff_reduce(l):
 
     return diff_reduce(newList) + l[len(l)-1]
 
+def diff_reduce_prev(l):
+    if is_all_equal(l):
+        return l[0]
+    newList = []
+    for index, value in enumerate(l[:-1]):
+        newList.append(l[index+1]-value)
+
+    return l[0] - diff_reduce_prev(newList)
+
 
 def main():
     df = read_file()
@@ -23,9 +32,11 @@ def main():
     df['numberList'] = df['data'].map(lambda x: x.split(' '))
     df['numberList'] = df['numberList'].map(lambda l: list(map(int, l)))
     df['nextValue'] = df['numberList'].map(diff_reduce)
+    df['previousValue'] = df['numberList'].map(diff_reduce_prev)
 
     print(df)
     print(df['nextValue'].sum())
+    print(df['previousValue'].sum())
 
 if __name__ == '__main__':
     main()
