6729c1d1143fa9e51295e4348394e3c9604d4388
Jesper Axelsson <jesper.axelsson@sogeti.se>
2024-12-02 11:06:57 +0100

    Day 2 of 2024 - Part 2

--- 2024/day2.py
+++ 2024/day2.py
@@ -8,7 +8,7 @@ def read_file():
 def check_list(s):
     l = s.split(' ')
     is_increasing = None
-    for i in range(0, len(l)-1):
+    for i in range(len(l)-1):
         [x,y] = l[i:i+2]
         diff = int(y)-int(x)
         abs_diff = abs(diff)
@@ -20,11 +20,41 @@ def check_list(s):
             return False
     return True
 
+def check_list_with_dampener(l):
+    is_increasing = None
+    dampener_used = False
+    for i in range(len(l)-1):
+        [x,y] = l[i:i+2]
+        diff = int(y)-int(x)
+        abs_diff = abs(diff)
+        if abs_diff >= 1 and abs_diff <= 3:
+            None #The data is where it supposed to be
+        else:
+            if dampener_used:
+                return False
+            else:
+                l[i+1] = x
+                dampener_used = True
+                continue
+        if is_increasing is None:
+            is_increasing = True if diff > 0 else False
+        if (is_increasing and diff < 0) or (not is_increasing and diff > 0):
+            if dampener_used:
+                return False
+            else:
+                l[i+1] = x
+                dampener_used = True
+    return True
+
 def main():
     df = read_file()
     df.columns = ['data']
     df['safeReport'] = df['data'].map(lambda x: check_list(x))
+    df['safeReportDampener'] = df['data'].map(lambda x: check_list_with_dampener(x.split(' ')))
+    df['reverseDampener'] = df[(df['safeReportDampener'] == False)]['data'].map(lambda x: check_list_with_dampener(x.split(' ')[::-1]))
     print(df['safeReport'].value_counts())
+    print(df['safeReportDampener'].value_counts())
+    print(df['reverseDampener'].value_counts())
     
 if __name__ == '__main__':
     main()
